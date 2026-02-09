83d2b6e277142b26a1b0e1eb69b52ec9b49007a4
Aditya Manas <manasaditya7907@gmail.com>
2026-02-10 02:37:00 +0530

    fix: add Toaster components to App.tsx to enable toast notifications

--- src/App.tsx
+++ src/App.tsx
@@ -177,6 +177,8 @@ const App = () => {
           </BrowserRouter>
         </GlobalErrorBoundary>
       </QueryClientProvider>
+      <Toaster />
+      <Sonner />
     </TooltipProvider>
   );
 };
